d44a456f395c122a5802b4b27c7bfd3faa836112
r.babakhani <babakhani.reza@gmail.com>
2015-02-14 17:40:09 +0330

    Implement angular routing

--- iWork.Web.UI/Content/js/page.js
+++ iWork.Web.UI/Content/js/page.js
@@ -1,122 +1,82 @@
 ﻿var iDropzone;
 Dropzone.autoDiscover = false;
-var iWork = angular.module('iWork', ['ngRoute', 'mgcrea.ngStrap', 'frapontillo.bootstrap-switch'])
-
-iWork.config(function ($routeProvider, $controllerProvider, $compileProvider, $filterProvider, $provide) {
-
-    iWork.controllerProvider = $controllerProvider;
-    iWork.compileProvider = $compileProvider;
-    iWork.routeProvider = $routeProvider;
-    iWork.filterProvider = $filterProvider;
-    iWork.provide = $provide;
-
+var iWork = angular.module('iWork', ['ngRoute', 'ngAnimate', 'mgcrea.ngStrap', 'frapontillo.bootstrap-switch']);
+iWork.config(function ($controllerProvider, $routeProvider, $compileProvider, $filterProvider, $provide) {
+    iWork.controller = $controllerProvider.register;
     $routeProvider
     .when('/contact/form', {
-        templateUrl: '/iView/Contacts/form.html',
-        controller: 'Contact_form'
-    }).when('/contact/grid', {
+        templateUrl: '/iView/Contacts/form.html'
+    })
+    .when('/contact/form/:entityId', {
+        templateUrl: '/iView/Contacts/form.html'
+    })
+    .when('/contact/grid', {
         templateUrl: '/iView/Contacts/grid.html',
+    })
+    .when('/kitchensink/form', {
+        templateUrl: '/iView/kitchensink/form.html',
+    })
+    .when('/kitchensink/grid-observer', {
+        templateUrl: '/iView/kitchensink/grid-observer.html',
+    })
+    .when('/kitchensink/grid-ajax', {
+        templateUrl: '/iView/kitchensink/grid-ajax.html',
+    })
+    .when('/kitchensink/form-validation', {
+        templateUrl: '/iView/kitchensink/form-validation.html',
     });
 });
 
+iWork.controller('submenu', function ($scope, $routeParams, $element, $rootScope, $http) {
+    $scope.$on('$routeChangeSuccess', function (scope, next, current) {
+        $('a.btn-iconic').each(function () {
+            $(this).removeClass('selected');
+            var thisUrl = $(this).attr("href");
+            if (window.location.hash.indexOf(thisUrl) >= 0) {
+                $(this).addClass('selected');
+            }
+        });
+    });
+});
 
 $(document).ready(function () {
+    $(document).on('confirmed.bs.confirmation', 'a.remove-entity', function () {
+        var $self = $(this);
+        var entityID = $self.attr('data-entity-id');
+        var entityController = $self.data('model');
+        $.ajax({
+            method: 'POST',
+            url: rootControllerUrl + entityController + "/remove",
+            data: {
+                id: entityID
+            },
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+        }).
+       success(function (responseData, status, headers, config) {
+           $self.parents('tr').remove();
+       }).
+       error(function (responseData, status, headers, config) {
+       });
+    });
     $('#navbar li').each(function () {
         var thisUrl = $(this).find('a').attr("href");
         if (window.location.href.indexOf(thisUrl) > 0) {
             $(this).addClass('active');
         }
     });
-
-    // Input Delete Script's
-    $(document).on('click', 'span.remove-entity', function () {
-        var entityID = $(this).data('entity-id');
-        var entityController = $(this).data('model');
-        var removeConfirm = $(this).data('confirm');
-        if (removeConfirm && removeConfirm !== 'false') {
-            var conf = confirm("Remove " + entityController + " " + entityID + "!");
-        } else {
-            var conf = true;
-        }
-        if (conf == true) {
-            $.ajax({
-                method: 'POST',
-                url: rootControllerUrl + entityController + "/remove",
-                data: {
-                    id: entityID
-                },
-                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-            }).
-           success(function (responseData, status, headers, config) {
-               console.log("success Ajax delete")
-               console.log(responseData)
-           }).
-           error(function (responseData, status, headers, config) {
-               console.log("error Ajax delete")
-               console.log(responseData)
-           });
+    $('a.btn-iconic').each(function () {
+        var thisUrl = $(this).attr("href");
+        if (window.location.hash.indexOf(thisUrl) >= 0) {
+            $(this).addClass('selected');
         }
     });
-    // END
-
-    // Input Update Script's
-    $(document).on('click', '.update-entity', function () {
-        var entityID = $(this).data('entity-id');
-        var formName = $(this).data('target-form');
-
-        var controllerElement = $('form[name=' + formName + ']').parents('[ng-controller]');
-        controllerElement.scope().formUpdate(entityID);
-    });
-    // END
-
-    /* Slick View Script's
-    =====================================================================================*/
-
-   
-    $('.slick-container').slick({
-        adaptiveHeight: true,
-        accessibility: false,
-        arrows: false,
-        edgeFriction: 10,
-        infinite: false,
-        swipe: false
-    }).on('afterChange', function () {
-        $('button[data-toggle=slick]').each(function () {
-            if ($(this).data("target") == $('.slick-container').slick('slickCurrentSlide')) {
-                $('[data-toggle]').removeClass('selected')
-                $(this).addClass('selected')
-            }
-        });
-    })
-    $('button[data-toggle=slick]').each(function () {
-        if ($(this).data("target") == $('.slick-container').slick('slickCurrentSlide')) {
-            $('[data-toggle]').removeClass('selected')
-            $(this).addClass('selected')
-        }
-    });
-
-    $('button[data-toggle=slick]').click(function () {
-        $('.slick-container').slick('slickGoTo', $(this).data("target"));
-
-        window.location.hash = $(this).data("target");
-
-        $('[data-toggle]').removeClass('selected')
-        $(this).addClass('selected');
-    });
-
-    if (window.location.hash !== '') {
-        $('.slick-container').slick('slickGoTo', parseInt(window.location.hash.match(/\d+/)[0]));
+    if (window.location.hash == '') {
+        $('a.btn-iconic').first().trigger('click');
     }
-    
-
-    //====================================================================================== END
-
+    $('.main-content').height($(window).height() - 100);
     $('.svg-inject').svgInject();
-
     $(window).resize(function () {
         $('.slick-list').height('auto');
+        $('.main-content').height($(window).height() - 100);
     });
-
-
-
 });
