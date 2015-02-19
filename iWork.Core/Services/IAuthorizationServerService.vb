5588361da4d6b9c4ee471b4f5ca6160412317861
Amir Pournasserian <amir.pournasserian@live.com>
2015-02-19 14:23:26 +0330

    Cleanup

--- iWork.Core/Services/IAuthorizationServerService.vb
+++ iWork.Core/Services/IAuthorizationServerService.vb
@@ -1,16 +1,16 @@
-﻿'Imports Microsoft.Owin.Security.OAuth
+﻿Imports Microsoft.Owin.Security.OAuth
 
-'Namespace Services
+Namespace Services
 
 
-'    Public Interface IAuthorizationServerService
-'        Inherits IService
+    Public Interface IAuthorizationServerService
+        Inherits IService
 
-'        Function ValidateClientAuthentication(context As OAuthValidateClientAuthenticationContext) As Task
-'        Function GrantResourceOwnerCredentials(context As OAuthGrantResourceOwnerCredentialsContext) As Task
-'        Function GrantRefreshToken(context As OAuthGrantRefreshTokenContext) As Task
-'        Function TokenEndpoint(context As OAuthTokenEndpointContext) As Task
+        Function ValidateClientAuthentication(context As OAuthValidateClientAuthenticationContext) As Task
+        Function GrantResourceOwnerCredentials(context As OAuthGrantResourceOwnerCredentialsContext) As Task
+        Function GrantRefreshToken(context As OAuthGrantRefreshTokenContext) As Task
+        Function TokenEndpoint(context As OAuthTokenEndpointContext) As Task
 
-'    End Interface
+    End Interface
 
-'End Namespace
+End Namespace
